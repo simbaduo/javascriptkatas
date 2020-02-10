832a338a3c411b4bf8e7f25444e038fd7403d5ae
Nathanael Swander <swander926@yahoo.com>
2020-02-09 21:01:49 -0500

    18. Senior Category

--- Solutions/src/functions.test.js
+++ Solutions/src/functions.test.js
@@ -267,6 +267,29 @@ const similarXO = (str) => {
   return str.match(/[x]/ig).length === str.match(/[o]/ig).length
 }
 
+/*
+ * 18) The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+ * They would like your help with an application form that will tell prospective members which category they will be placed.
+ * To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+ * Input
+ * Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+ * Note for F#: The input will be of (int list list) which is a List<List>
+ * Example Input
+ * [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+ * Output
+ * Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+ * Example Output
+ * ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+*/
+
+function validSenior (data) {
+  return data.map(person => {
+    return person[0] >= 55 && person[1] > 7
+      ? 'Senior' : 'Open'
+  })
+}
+
 /**
  * NOTE: Don't modify anything below this line...
  */
@@ -520,6 +543,13 @@ test('similarXO()', t => {
   t.deepEqual(similarXO('Oo'), false)
   t.deepEqual(similarXO('ooom'), false)
   t.deepEqual(similarXO('xxxoooo'), false)
+})
+
+test('Function Check - Categorize New Member', t => ensureDefined(t, 'validSenior'))
+test('validSenior()', t => {
+  t.deepEqual(validSenior([[45, 12], [55, 21], [19, -2], [104, 20]]), ['Open', 'Senior', 'Open', 'Senior'])
+  t.deepEqual(validSenior([[3, 12], [55, 1], [91, -2], [54, 23]]), ['Open', 'Open', 'Open', 'Open'])
+  t.deepEqual(validSenior([[59, 12], [55, -1], [12, -2], [12, 12]]), ['Senior', 'Open', 'Open', 'Open'])
 })
 
 /* eslint-enable */
